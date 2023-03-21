b7d0d18ba1087f32f7bf06c4bd411f42f0b5377f
fcegatti <fcegatti@yahoo.es>
2023-03-21 13:49:51 +0100

    unrelated: banner for GitHub profile

---
+++ about.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+const aboutMe = {
+  name: "federico gatti",
+  role: "fullstack developer with linguistic and communication skills",
+  data: { 
+      linkedin: "www.linkedin.com/in/fcegatti",
+      gitHub: "https://github.com/fcegatti", 
+  },            
+  languages: ["Español", "English", "Català", "Français", "Italiano", "Ελλινικά", "Português", "Avañe'ẽ"],
+  techStack: ["HTML", "CSS", "JavaScript", "React", "PHP", "Laravel", "MySQL"],
+};
+
+
+
+
+
+
+
+
+
